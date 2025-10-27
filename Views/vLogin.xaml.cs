5d5028300c904474f2ce9a1654faca264a3c91c0
Jazmin <e1725897548@uisrael.edu.ec>
2025-10-27 12:05:34 -0500

    Validación tarea semana 3

--- Views/vLogin.xaml.cs
+++ Views/vLogin.xaml.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+
 namespace ojherreras2.Views;
 
 public partial class vLogin : ContentPage
@@ -14,8 +16,34 @@ public partial class vLogin : ContentPage
 		InitializeComponent();
 	}
 
-    private async void Button_Clicked(object sender, EventArgs e)
+    private async Task btnIngresar_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            string usuario = txtUsuario.Text?.Trim();
+            string contrasena = txtContrasena.Text?.Trim();
+
+            if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(contrasena))
+            {
+                await DisplayAlert("Error", "Debe ingresar usuario y contraseña correctos", "Ok");
+                return;
+            }
+
+            if (usuariosValidos.TryGetValue(usuario, out string passwordcorrecto) && passwordcorrecto == contrasena)
+            {
+                await DisplayAlert("Acceso concedido", $"Bienvenido {usuario}", "continuar");
+                await Navigation.PushAsync(new vInicio());
+            }
+            else
+            {
+                await DisplayAlert("error", "Usuario o contraseña incorrectos", "ok");
+            }
+        }
+
+        catch (Exception ex) {
+            Console.WriteLine($"Error en login: {ex.Message}");
+            await DisplayAlert("Error", "ocurrio un problema", "ok");
+        }
 
     }
 }
